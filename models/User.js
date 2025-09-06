ebbb3ec5281e94bc124f41fbcf963503d9f64f3d
jSSjin <jerat004@gmail.com>
2025-09-06 21:21:31 +0700

    add userID in User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema({
+  userID: {
+    type: String,
+    require: true,
+    unique: true
+  },
   name: {
     type: String,
     required: true
