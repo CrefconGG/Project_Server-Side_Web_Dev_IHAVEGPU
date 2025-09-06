9250456bd1440bc46b920577e8311085c70bbfbe
CrefconGG <miss.sunisa1978@gmail.com>
2025-09-06 21:31:15 +0700

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -32,7 +32,7 @@ const OrderSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['pending', 'shipped', 'delivered', 'cancelled'],
+        enum: ['pending', 'canceled', 'paid'],
         default: 'pending'
     }
 })
