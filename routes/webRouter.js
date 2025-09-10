6589a4b96188e1b75f4ff1106d930a89e33f7098
CrefconGG <miss.sunisa1978@gmail.com>
2025-09-10 22:02:07 +0700

    Update webRouter.js

--- routes/webRouter.js
+++ routes/webRouter.js
@@ -2,7 +2,7 @@ import express from 'express'
 import productController from "../controllers/productController.js"
 const router = express.Router()
 
-router.get('/products', productController.getProductView);
+router.get('/', productController.getProductView);
 router.get('/products/:id', productController.getDetailsView);
 
 
