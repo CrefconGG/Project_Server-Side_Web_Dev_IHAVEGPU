828060a6a951f3659df9b3e165b3920380606d6b
jSSjin <jerat004@gmail.com>
2025-09-25 00:40:09 +0700

    change role for accessing cartRoutes

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,11 +2,11 @@ import cartController from "../controllers/cartController.js";
 import authMiddleware from "../middlewares/authMiddleware.js"
 
 const useCartRoute = async (router) => {
-  router.get("/cart/:userID", authMiddleware("user"), cartController.getCartByUserId);
-  router.post("/cart/:userID/item", authMiddleware("user"), cartController.addItem);
-  router.delete("/cart/:userID/item/:productID", authMiddleware("user"), cartController.removeItem);
-  router.put("/cart/:userID/item/:productID", authMiddleware("user"), cartController.updateItemQuantity);
-  router.delete("/cart/:userID",authMiddleware("user"), cartController.clearCart);
+  router.get("/cart/:userID", authMiddleware(), cartController.getCartByUserId);
+  router.post("/cart/:userID/item", authMiddleware(), cartController.addItem);
+  router.delete("/cart/:userID/item/:productID", authMiddleware(), cartController.removeItem);
+  router.put("/cart/:userID/item/:productID", authMiddleware(), cartController.updateItemQuantity);
+  router.delete("/cart/:userID",authMiddleware(), cartController.clearCart);
 };
 
 export default useCartRoute;
