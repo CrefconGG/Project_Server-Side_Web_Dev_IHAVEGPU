3f0c4f48763edcb37127345387fe891dab405cf6
jSSjin <jerat004@gmail.com>
2025-09-25 00:26:57 +0700

    cart api cart working  properly in backend

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,11 +1,12 @@
 import cartController from "../controllers/cartController.js";
+import authMiddleware from "../middlewares/authMiddleware.js"
 
 const useCartRoute = async (router) => {
-  router.get("/cart/:userID", cartController.getCartByUserId);
-  router.post("/cart/:userID/item", cartController.addItem);
-  router.delete("/cart/:userID/item/:productID", cartController.removeItem);
-  router.put("/cart/:userID/item/:productID", cartController.updateItemQuantity);
-  router.delete("/cart/:userID", cartController.clearCart);
+  router.get("/cart/:userID", authMiddleware("user"), cartController.getCartByUserId);
+  router.post("/cart/:userID/item", authMiddleware("user"), cartController.addItem);
+  router.delete("/cart/:userID/item/:productID", authMiddleware("user"), cartController.removeItem);
+  router.put("/cart/:userID/item/:productID", authMiddleware("user"), cartController.updateItemQuantity);
+  router.delete("/cart/:userID",authMiddleware("user"), cartController.clearCart);
 };
 
 export default useCartRoute;
