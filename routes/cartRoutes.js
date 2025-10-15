e2255554845461148fc2bce2392be145a2df76f0
TunaOfFinality <66070071@kmitl.ac.th>
2025-10-15 23:29:11 +0700

    update removeitem for cart.ejs

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,11 +2,11 @@ import cartController from "../controllers/cartController.js";
 import authMiddleware from "../middlewares/apiMiddleware.js"
 
 const useCartRoute = async (router) => {
-  router.get("/cart", authMiddleware({ allowedRoles: ["admin"] }), cartController.getCartByUserId);
-  router.post("/cart/items", authMiddleware({ allowedRoles: ["admin"] }), cartController.addItem);
-  router.delete("/cart/items/:productID", authMiddleware({ allowedRoles: ["admin"] }), cartController.removeItem);
-  router.put("/cart/items/:productID", authMiddleware({ allowedRoles: ["admin"] }), cartController.updateItemQuantity);
-  router.delete("/cart", authMiddleware({ allowedRoles: ["admin"] }), cartController.clearCart);
+  router.get("/cart", authMiddleware({ allowedRoles: ["admin","user"] }), cartController.getCartByUserId);
+  router.post("/cart/items", authMiddleware({ allowedRoles: ["admin","user"] }), cartController.addItem);
+  router.delete("/cart/items/:productID", authMiddleware({ allowedRoles: ["admin","user"] }), cartController.removeItem);
+  router.put("/cart/items/:productID", authMiddleware({ allowedRoles: ["admin","user"] }), cartController.updateItemQuantity);
+  router.delete("/cart", authMiddleware({ allowedRoles: ["admin","user"] }), cartController.clearCart);
 };
 
 export default useCartRoute;
