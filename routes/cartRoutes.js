95cb01c756474bdf56f2ac1b2e5d11430089e98a
TunaOfFinality <66070071@kmitl.ac.th>
2025-10-15 23:52:32 +0700

    add update quantity and clear cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,11 +2,11 @@ import cartController from "../controllers/cartController.js";
 import authMiddleware from "../middlewares/apiMiddleware.js"
 
 const useCartRoute = async (router) => {
-  router.get("/cart", authMiddleware({ allowedRoles: ["admin","user"] }), cartController.getCartByUserId);
-  router.post("/cart/items", authMiddleware({ allowedRoles: ["admin","user"] }), cartController.addItem);
-  router.delete("/cart/items/:productID", authMiddleware({ allowedRoles: ["admin","user"] }), cartController.removeItem);
-  router.put("/cart/items/:productID", authMiddleware({ allowedRoles: ["admin","user"] }), cartController.updateItemQuantity);
-  router.delete("/cart", authMiddleware({ allowedRoles: ["admin","user"] }), cartController.clearCart);
+  router.get("/cart", authMiddleware(), cartController.getCartByUserId);
+  router.post("/cart/items", authMiddleware(), cartController.addItem);
+  router.delete("/cart/items/:productID", authMiddleware(), cartController.removeItem); 
+  router.put("/cart/items/:productID", authMiddleware(), cartController.updateItemQuantity);
+  router.delete("/cart", authMiddleware(), cartController.clearCart);
 };
 
 export default useCartRoute;
